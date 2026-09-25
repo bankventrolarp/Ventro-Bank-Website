fc6cfb3e0340af167135bb814185860a42bd7c5e
NYTRIX PRIME <nytrixprime@gmail.com>
2026-09-25 01:39:21 -0400

    Build Ventro Bank website: add config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+export const SITE = {
+  name: "Ventro Bank",
+  tagline: "Your Money. Your Vault. Your World.",
+  discordUrl: "https://discord.gg/dJ8aGdGapq",
+  androidPlayUrl: "",
+  iosUrl: "",
+  androidStatus: "AVAILABLE NOW",
+  iosStatus: "COMING SOON"
+};
